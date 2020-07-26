5429faa5320f9218a6d15c4703d9c87862bac056
sst7 <sst7@rice.edu>
2020-07-26 12:47:52 -0500

    Fixed modal javascript code

--- index.js
+++ index.js
@@ -6,42 +6,37 @@ var r_modal = $("#modal-rice");
 var btnIMSA = $("#btn-imsa");
 var btnRice = $("#btn-rice");
 
-// Get the <span> element that closes the modal
-var i_span = $(".close")[0];
-var r_span = $(".close")[1];
-
 //Global variable that stores which button was activated
 var active_btn = null;
 
 // When the user clicks the button, open the modal
-btnIMSA.onclick = function() {
-  i_modal.style.display = "block";
-  active_btn = i_modal;
-}
-
-btnRice.onclick = function() {
-  r_modal.style.display = "block";
+btnIMSA.click(function() {
+  i_modal.css("display","block");
   active_btn = r_modal;
-}
+});
+
+btnRice.click(function() {
+  r_modal.css("display","block");
+});
 
 // When the user clicks on <span> (x), close the modal
-i_span.onclick = function() {
-  active_btn.style.display = "none";
-}
+i_modal.on("click", ".close", function() {
+  i_modal.css("display", "none");
+});
 
 // When the user clicks on <span> (x), close the modal
-r_span.onclick = function() {
-  active_btn.style.display = "none";
-}
+r_modal.on("click", ".close", function() {
+  r_modal.css("display", "none");
+});
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == active_btn) {
-    active_btn.style.display = "none";
+$(window).click(function(event) {
+  if ($(event.target).is(active_btn)) {
+    active_btn.css("display", "none");
   }
-}
+});
 
-$(document).on("scroll", function() {
+$(document).scroll(function() {
   var pageTop = $(document).scrollTop()
   var pageBottom = pageTop + $(window).height()
   var tags = $(".job")
@@ -51,13 +46,15 @@ $(document).on("scroll", function() {
       $(tag).addClass("job-fade")
     }
   }
-})
+});
 
 // When window size is small, loop through buttons and decrease size and padding
 $(window).resize(function(){
   if ($(window).width() <= 540) {
     $(".title-buttons").removeClass("fa-3x").addClass("fa-2x");
     $(".title-buttons").css("padding", "5px");
+    $(".school-pic").css("display", "none");
+
   }
   else {
     $(".title-buttons").removeClass("fa-2x").addClass("fa-3x");
